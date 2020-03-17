986c4f0e32b56efbd054126bfcff35a88305e903
Evan Cole Wright <35124669+evancolewright@users.noreply.github.com>
2020-03-16 23:42:16 -0400

    3/17/20

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
+import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Navbar from "./components/layout/Navbar";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
